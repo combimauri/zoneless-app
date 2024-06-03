79f19d701b597df2219c96dd0fa95613f17cd701
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2024-06-02 23:55:18 -0400

    Zone implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,67 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+
+interface Member {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [AsyncPipe],
+  template: `
+    <h3>Property update in an event</h3>
+    <p>Counter: {{ counter }}</p>
+    <button (click)="incrementCounter()">Increment counter</button>
+    <hr />
+
+    <!-- <h3>Property update asynchronously</h3>
+    <p>Tick: {{ tick }}</p>
+    <hr /> -->
+
+    <!-- <h3>Property update from an observable</h3>
+    @for (member of members; track member.id) {
+    <p>
+      {{ member.name }}
+    </p>
+    }
+    <hr /> -->
+
+    <!-- <h3>Property update from an observable using async pipe</h3>
+    @for (member of members$ | async; track member.id) {
+    <p>
+      {{ member.name }}
+    </p>
+    }
+    <hr /> -->
+  `,
 })
-export class AppComponent {
-  title = 'zoneless-app';
+export class AppComponent implements OnInit, OnDestroy {
+  readonly #API_URL = 'https://jsonplaceholder.typicode.com/users';
+  readonly #http = inject(HttpClient);
+
+  counter = 0;
+
+  tick = 0;
+
+  members: Member[] = [];
+  #membersSub = this.#http
+    .get<Member[]>(this.#API_URL)
+    .subscribe((members) => (this.members = members));
+
+  members$ = this.#http.get<Member[]>(this.#API_URL);
+
+  ngOnInit(): void {
+    setInterval(() => this.tick++, 1000);
+  }
+
+  ngOnDestroy(): void {
+    this.#membersSub.unsubscribe();
+  }
+
+  incrementCounter() {
+    this.counter++;
+  }
 }
